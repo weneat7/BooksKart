3d76787b4daf9541a5f1a4009813d43fa62373aa
Vineet Singh Rajput <vineetsinghrajput@Vineets-Mac-mini.local>
2024-01-15 18:06:32 +0530

    first commit

---
+++ src/main/java/com/example/bookskart/BooksKartApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookskart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksKartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksKartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookskart/BooksKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookskart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksKartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
